a524307826a32e1d20da27c0c58d58a183c08527
Alexander Lairich <alexanderlairich@web.de>
2024-05-16 09:17:52 +0200

    Cleaned authentication code

--- Pocketsharp/Authentication.cs
+++ Pocketsharp/Authentication.cs
@@ -1,8 +1,6 @@
-﻿/*
- * Note: Set up the base URL for your HttpClient instance before passing it as a parameter; otherwise, null will be returned.
- */
-
-using Pocketsharp.Objects;
+﻿using Pocketsharp.Objects;
+using Pocketsharp.Utility;
+using System;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
@@ -12,6 +10,9 @@ namespace Pocketsharp
     {
         public class EmailAndPassword
         {
+            static readonly string registerApiEndpoint = "/api/collections/users/records";
+            static readonly string loginApiEndpoint = "/api/collections/users/auth-with-password";
+
             /// <summary>
             /// Register a new user and return an AuthRecord object upon successful registration
             /// </summary>
@@ -28,19 +29,8 @@ namespace Pocketsharp
             {
                 try
                 {
-                    if (string.IsNullOrEmpty(client.BaseAddress?.ToString()))
-                        throw new NotImplementedException("Setup the base address on the client");
-
-                    if (string.IsNullOrEmpty(record.Email))
-                        throw new NotImplementedException("A email is required for the registration process");
-
-                    if (string.IsNullOrEmpty(password))
-                        throw new NotImplementedException("A password is required for the registration process");
-
-                    if (string.IsNullOrEmpty(passwordConfirm))
-                        throw new NotImplementedException("A password conformation is required for the registration process");
-
-                    string apiEndpoint = "/api/collections/users/records";
+                    if (!InputValidation.RegistrationInputIsValid(client, record, password, passwordConfirm))
+                        throw new NotImplementedException($"LIBRARY ERROR\n{"Register input is not valid"}");
 
                     var requestbody = new
                     {
@@ -53,7 +43,7 @@ namespace Pocketsharp
                         passwordConfirm
                     };
 
-                    var response = await client.PostAsJsonAsync(apiEndpoint, requestbody);
+                    var response = await client.PostAsJsonAsync(registerApiEndpoint, requestbody);
                     return await response.Content.ReadFromJsonAsync<Record>();
                 }
                 catch (Exception exception)
@@ -73,16 +63,8 @@ namespace Pocketsharp
             {
                 try
                 {
-                    if (string.IsNullOrEmpty(client.BaseAddress?.ToString()))
-                        throw new NotImplementedException("Setup the base address on the client");
-
-                    if (string.IsNullOrEmpty(email))
-                        throw new NotImplementedException("A email is required for the login process");
-
-                    if (string.IsNullOrEmpty(password))
-                        throw new NotImplementedException("A password is required for the login process");
-
-                    string apiEndpoint = "/api/collections/users/auth-with-password";
+                    if (!InputValidation.LoginInputIsValid(client, email, password))
+                        throw new NotImplementedException($"LIBRARY ERROR\n{"Login input is not valid"}");
 
                     var requestBody = new
                     {
@@ -90,7 +72,7 @@ namespace Pocketsharp
                         password
                     };
 
-                    var response = await client.PostAsJsonAsync(apiEndpoint, requestBody);
+                    var response = await client.PostAsJsonAsync(loginApiEndpoint, requestBody);
                     return await response.Content.ReadFromJsonAsync<Response>();
                 }
                 catch (Exception exception)
---
+++ Pocketsharp/Utility/InputValidation.cs
@@ -0,0 +1,43 @@
+﻿using Pocketsharp.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pocketsharp.Utility
+{
+    internal class InputValidation
+    {
+        public static bool RegistrationInputIsValid(HttpClient client, Record record, string password, string passwordConfirm)
+        {
+            if (string.IsNullOrEmpty(client.BaseAddress?.ToString()))
+                throw new NotImplementedException("Setup the base address on the client");
+
+            if (string.IsNullOrEmpty(record.Email))
+                throw new NotImplementedException("A email is required for the registration process");
+
+            if (string.IsNullOrEmpty(password))
+                throw new NotImplementedException("A password is required for the registration process");
+
+            if (string.IsNullOrEmpty(passwordConfirm))
+                throw new NotImplementedException("A password conformation is required for the registration process");
+
+            return true;
+        }
+
+        public static bool LoginInputIsValid(HttpClient client, string email, string password)
+        {
+            if (string.IsNullOrEmpty(client.BaseAddress?.ToString()))
+                throw new NotImplementedException("Setup the base address on the client");
+
+            if (string.IsNullOrEmpty(email))
+                throw new NotImplementedException("A email is required for the registration process");
+
+            if (string.IsNullOrEmpty(password))
+                throw new NotImplementedException("A password is required for the registration process");
+
+            return true;
+        }
+    }
+}
